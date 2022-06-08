c919c1003505a0d04e29863ed895a620a667349c
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-08 15:45:09 -0700

    98 battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from input to 98
+ * @n: number to start from
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
