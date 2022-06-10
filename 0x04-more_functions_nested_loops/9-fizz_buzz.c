ca0cd8e3bd5383ee0e519804a6fc7c88984639bd
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-10 07:20:44 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) == 0))
+		if (i % 3 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz\n");
 	}
-	else if ((i % 3) == 0)
+	else if (i % 3 == 0)
 	{
-		printf("Fizz");
+		printf("Fizz\n");
 	}
-	else if ((i % 5) == 0)
+	else if (i % 5 == 0)
 	{
-		printf("Buzz");
+		printf("Buzz\n");
 	}
 	else
 	{
-		printf("%d", i);
+		printf("%d\n", i);
 	}
 }
 
