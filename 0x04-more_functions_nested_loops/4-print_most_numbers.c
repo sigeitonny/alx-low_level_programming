b5e35b3afad036c57dffc7b6e716223cb7c056c2
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-10 05:28:19 -0700

    numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num <= 9; num++)
+	for (i = 0; i <= 9; i++)
+
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
+	if (i != 2 && i != 4)
+	{
+		_putchar(i + '0');
+	}
 	}
 
 	_putchar('\n');
