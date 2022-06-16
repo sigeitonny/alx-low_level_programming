b5910200f7743caadd5a01f81cf6771df8c1d2f0
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-16 03:33:21 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if equal,
+ * negative 0 if s1 < s2,
+ * psitive 0 if s1 > s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
+	}
+	return (0);
+}
