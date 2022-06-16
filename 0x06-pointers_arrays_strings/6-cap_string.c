157b0f14e1427a855339177435fb7f1a86651eb2
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-16 06:59:28 -0700

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes words of a string.
+ * @s: string to be capitalized.
+ *
+ * Return: pointer to the string.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+				|| s[i] ==  '"' || s[i] == '(' || s[i] == ')' || s[i] == '{'
+				|| s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
