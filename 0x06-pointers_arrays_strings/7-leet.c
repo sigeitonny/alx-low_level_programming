f05e7a4aad2efac5868ec3511b9f179c3101d3f3
sigeitonny <teiguttonnykipkemoi@gmail.com>
2022-06-16 08:45:25 -0700

    Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,20 @@
  */
 char *leet(char *str)
 {
-	int i = 0;
+	int i = 0, j;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[i])
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; j <= 7; j++)
+		{
+			if (str[i] == leet[j] ||
+					str[i] - 32 == leet[j])
+				str[i] = j + '0';
+		}
+
 		i++;
 	}
+
 	return (str);
 }
